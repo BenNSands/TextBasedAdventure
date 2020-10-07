5a47c3c7d74928ba62ef03d22fc8a3826e916652
BenSands <m3gap0ky0@gmail.com>
2020-10-07 13:25:15 -0800

    added give player sword string

--- CommandGame/Program.cs
+++ CommandGame/Program.cs
@@ -8,6 +8,7 @@ namespace CommandGame
         {
             Console.WriteLine("Hello Slayer! What's your name?");
             var playerName = Console.ReadLine();
+            Console.WriteLine("Here is your " + playerName + " Sword! Now get out there and do what you do best!");
         }
     }
 }
