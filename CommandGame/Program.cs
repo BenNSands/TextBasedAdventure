c6066beb77b8de2b5c84b61279568119137bcb67
BenSands <m3gap0ky0@gmail.com>
2020-10-07 13:27:36 -0800

    fixed spelling error

--- CommandGame/Program.cs
+++ CommandGame/Program.cs
@@ -8,7 +8,7 @@ namespace CommandGame
         {
             Console.WriteLine("Hello Slayer! What's your name?");
             var playerName = Console.ReadLine();
-            Console.WriteLine("Here is your " + playerName + " Sword! Now get out there and do what you do best!");
+            Console.WriteLine("Here is your Sword " + playerName + "! Now get out there and do what you do best!");
         }
     }
 }
