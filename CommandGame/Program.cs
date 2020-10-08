353229e5283ea8daa5abb1e820f2071c2a92815c
BenSands <m3gap0ky0@gmail.com>
2020-10-08 14:44:28 -0800

    added HP MP

--- CommandGame/Program.cs
+++ CommandGame/Program.cs
@@ -9,6 +9,11 @@ namespace CommandGame
             Console.WriteLine("Hello Slayer! What's your name?");
             var playerName = Console.ReadLine();
             Console.WriteLine("Here is your Sword " + playerName + "! Now get out there and do what you do best!");
+            int HP = 100;
+            int MP = 50;
+            Console.WriteLine("HP: " + HP);
+            Console.WriteLine("MP: " + MP);
+
         }
     }
 }
