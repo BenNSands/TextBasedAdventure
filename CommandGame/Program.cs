d94502cdb14e18171fc1369d82b000ab5157c1c0
BenSands <m3gap0ky0@gmail.com>
2020-10-08 14:56:03 -0800

    forgot to save before last commit

--- CommandGame/Program.cs
+++ CommandGame/Program.cs
@@ -13,6 +13,21 @@ namespace CommandGame
             int MP = 50;
             Console.WriteLine("HP: " + HP);
             Console.WriteLine("MP: " + MP);
+            Console.WriteLine("*You are faced with two door, which do you choose?*");
+            Console.WriteLine("1.Enter to follow the path of the Dragon");
+            Console.WriteLine("2. Enter to follow the path of the Lich Lord");
+            var firstChoice = Console.ReadLine();
+            if ()
+            {
+
+            } else if()
+            {
+
+            }
+            else
+            {
+
+            }
 
         }
     }
