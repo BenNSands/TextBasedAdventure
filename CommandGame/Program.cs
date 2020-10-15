51fa2e79b69db244b3431a004c231ab3e76ad418
Ben Sands <m3gap0ky0@gmail.com>
2020-10-14 21:34:43 -0600

    added some comments

--- CommandGame/Program.cs
+++ CommandGame/Program.cs
@@ -9,14 +9,19 @@ namespace CommandGame
             Console.WriteLine("Hello Slayer! What's your name?");
             var playerName = Console.ReadLine();
             Console.WriteLine("Here is your Sword " + playerName + "! Now get out there and do what you do best!");
+            
+            //declaring hp and mp
             int HP = 100;
             int MP = 50;
             Console.WriteLine("HP: " + HP);
             Console.WriteLine("MP: " + MP);
-            Console.WriteLine("*You are faced with two door, which do you choose?*");
+            Console.WriteLine("*You are faced with two doors, which do you choose?*");
+
+            // First fork in the road
             Console.WriteLine("1.Enter to follow the path of the Dragon   *COMBAT*");
             Console.WriteLine("2. Enter to follow the path of the Lich Lord   *PUZZLES*");
             var firstChoice = Console.ReadLine();
+
             if (firstChoice == "1")
             {
                 Console.WriteLine("You enter the room, and are greeted by a Rat with a knife in it's mouth!");
