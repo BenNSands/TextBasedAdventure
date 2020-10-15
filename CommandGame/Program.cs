d1b8f4aa5f5589382635118e9387dcd2a84054cb
Ben Sands <m3gap0ky0@gmail.com>
2020-10-14 21:52:06 -0600

    added new dialouge and minor grammar fixes

--- CommandGame/Program.cs
+++ CommandGame/Program.cs
@@ -18,22 +18,32 @@ namespace CommandGame
             Console.WriteLine("*You are faced with two doors, which do you choose?*");
 
             // First fork in the road
-            Console.WriteLine("1.Enter to follow the path of the Dragon   *COMBAT*");
-            Console.WriteLine("2. Enter to follow the path of the Lich Lord   *PUZZLES*");
+            Console.WriteLine("1 - Enter to follow the path of the Dragon   *COMBAT*");
+            Console.WriteLine("2 - Enter to follow the path of the Lich Lord   *PUZZLES*");
             var firstChoice = Console.ReadLine();
 
             if (firstChoice == "1")
             {
-                Console.WriteLine("You enter the room, and are greeted by a Rat with a knife in it's mouth!");
+                Console.WriteLine("You enter the room, and are greeted by a Rat holding a knife!");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Attack the Rat with your Sword ");
+                Console.WriteLine("2 - Punch the Rat");
+                var ratChoice = Console.ReadLine();
+                //if or switch statement here
 
             } else if (firstChoice == "2")
             {
                 Console.WriteLine("You enter the room, and hear an eerie noise in the walls, there is a hole in the wall leading into a cave.");
-
+                Console.WriteLine("");
+                Console.WriteLine("1 - Investigate the noise");
+                Console.WriteLine("2 - Head straight to the exit");
             }
             else
             {
-                Console.WriteLine("*You found a Secret Room! You aren't supposed to be here!");
+                Console.WriteLine("You fall into a hidden pit and take 99 DMG");
+                HP = 1;
+                Console.WriteLine($"HP: {HP}");
+                // need to continue with something here
             }
 
         }
